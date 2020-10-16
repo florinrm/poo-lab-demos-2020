f67eb8e1ae8e41319092337a59a4129eced26e83
Florin Mihalache <florinrmfm3@gmail.com>
2020-10-16 12:53:53 +0300

    Added equals(), hashCode() and toString() generated overrides

--- src/lab1/Song.java
+++ src/lab1/Song.java
@@ -1,5 +1,7 @@
 package lab1;
 
+import java.util.Objects;
+
 public class Song {
     private String artist;
     private String title;
@@ -33,5 +35,29 @@ public class Song {
         System.out.println("Title: " + title);
         System.out.println("Artist: " + artist);
         System.out.println("Year: " + year);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Song)) return false;
+        Song song = (Song) o;
+        return year == song.year &&
+                Objects.equals(artist, song.artist) &&
+                Objects.equals(title, song.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(artist, title, year);
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "artist='" + artist + '\'' +
+                ", title='" + title + '\'' +
+                ", year=" + year +
+                '}';
     }
 }
